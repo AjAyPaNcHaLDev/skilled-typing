f859d305d04617effaf2dff22a2c81504ff7ba23
ajay panchal <ap8570091@gmail.com>
2023-08-08 16:45:08 +0530

    deploy

--- src/app/page.js
+++ src/app/page.js
@@ -12,23 +12,25 @@ const page = () => {
         <WelcomeToast />
         <Box>
           {/* <div className="j-center flex-col gap-10 .f-dir-col"> */}
-          <div className=" ">
-            <h1 className={box.h4}>
+          <div className="flex flex-col gap-12 items-center align-middle justify-center ">
+            <h1 className={box.h4 + " font-semibold "}>
               <AnimantedText />
               <span
                 id="cursor"
                 style={{ borderLeft: ".1em solid rgb(16, 96, 233)", height: 2 }}
               ></span>
             </h1>
+            <div>
+              <Link
+                href="/Login"
+                className={
+                  "my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold p-3  rounded-full "
+                }
+              >
+                Let's Start
+              </Link>
+            </div>
           </div>
-          <Link
-            href="/Login"
-            className={
-              " bg-blue-500 hover:bg-blue-700 text-white font-bold p-3  rounded-full "
-            }
-          >
-            Let's Start
-          </Link>
         </Box>
       </div>
     </>
